11f09862dcd06eb80c245370564b4ff352fb166d
a.lamour <dev.inf@hotmail.fr>
2022-03-16 18:02:58 +0100

    handle the logout action

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -1,36 +1,23 @@
 <template>
   <v-app-bar app dark>
-    <nuxt-link
-      v-if="isAuthenticated"
-      id="link-to-home"
-      class="navbar-item"
-      to="/home"
-    >
+    <nuxt-link id="link-to-home" class="navbar-item" to="/home">
       <v-btn> PoliScrypts </v-btn>
     </nuxt-link>
     <v-spacer />
-    <nuxt-link
-      v-if="isAuthenticated"
-      id="link-to-entreprises"
-      class="navbar-item"
-      to="/entreprise"
-    >
+    <nuxt-link id="link-to-entreprises" class="navbar-item" to="/entreprise">
       <v-btn to="/entreprise" text> Entreprises </v-btn>
     </nuxt-link>
-    <nuxt-link
-      v-if="isAuthenticated"
-      id="link-to-contacts"
-      class="navbar-item"
-      to="/contact"
+    <nuxt-link id="link-to-contacts" class="navbar-item" to="/contact"
       ><v-btn> Contacts </v-btn>
     </nuxt-link>
     <v-spacer />
-    <v-menu v-if="isAuthenticated" offset-y>
+    <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
         <v-icon medium color="white darken-2" v-on="on" v-bind="attrs">
           mdi-account
         </v-icon>
       </template>
+
       <v-card>
         <v-card-title class="primary white--text">Profile</v-card-title>
         <v-divider />
@@ -38,8 +25,10 @@
           <span class="text-h6">{{ username }}</span>
         </v-card-text>
         <v-card-actions>
-          <v-btn color="blue darken-1" @click="logout">Logout</v-btn>
-        </v-card-actions>
+          <v-btn text color="blue darken-1" @click="logout"
+            >Logout</v-btn
+          ></v-card-actions
+        >
       </v-card>
     </v-menu>
   </v-app-bar>
@@ -55,14 +44,15 @@ export default {
   computed: {
     ...mapGetters({
       username: "profile/getUsername",
-      isAuthenticated: "profile/isAuthenticated",
     }),
   },
-  method: {
+  methods: {
     logout() {
-      alert("logout");
-      this.$store.dispatch("profile/logout");
-      this.$router.push("auth");
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      localStorage.removeItem("username");
+      localStorage.removeItem("roles");
+      this.$router.push("/auth");
     },
   },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,15 @@
 <template>
-    <v-row justify="center" align="center">
-      <v-app-bar app dark>
-        <v-spacer/>
-      <nuxt-link
-        id="link-to-login"
-        class="navbar-item"
-        to="/auth"
+  <v-row justify="center" align="center">
+    <v-app-bar app dark>
+      <v-spacer />
+      <nuxt-link id="link-to-login" class="navbar-item" to="/auth"
         ><v-btn> Connexion </v-btn>
       </nuxt-link>
     </v-app-bar>
-      <v-col cols="12" class="text-center">
-        <h1>Welcome to PoliScrypts mini project</h1>
-      </v-col>
-    </v-row>
+    <v-col cols="12" class="text-center">
+      <h1>Welcome to PoliScrypts mini project</h1>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
